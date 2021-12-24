4b773e30e94273ab85c1abb1200cff89fd7f356c
Patrick NIYONGIRA <niyongirapatrick29@gmail.com>
2021-12-24 17:16:59 +0200

    Static files

--- manage.py
+++ manage.py
@@ -23,26 +23,25 @@ if __name__ == '__main__':
 '''
 
 #!/usr/bin/env python
- """Django's command-line utility for administrative tasks."""
- import os
- import sys
+"""Django's command-line utility for administrative tasks."""
+import os
+import sys
 
- def main():
-     """Run administrative tasks."""
-     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 
-'your_project.settings')
-     try:
-         from django.core.management import execute_from_command_line
-     except ImportError as exc:
-         raise ImportError(
-             "Couldn't import Django. Are you sure it's installed and "
-             "available on your PYTHONPATH environment variable? Did you "
-             "forget to activate a virtual environment?"
-         ) from exc
-     try:
-          execute_from_command_line(sys.argv) # just put this in try block
-     except:
-          pass
+def main():
+    """Run administrative tasks."""
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE','your_project.settings')
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+    try:
+        execute_from_command_line(sys.argv) # just put this in try block
+    except:
+        pass
 
- if __name__ == '__main__':
-     main()
+if __name__ == '__main__':
+    main()
