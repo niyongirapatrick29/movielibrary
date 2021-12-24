08825a2a177712e1fe8ae29bc4563c1bd3397929
Patrick NIYONGIRA <niyongirapatrick29@gmail.com>
2021-12-24 17:24:17 +0200

    Static files

--- movies/settings.py
+++ movies/settings.py
@@ -161,7 +161,20 @@ STATICFILES_DIRS = [
 STATIC_URL = '/static/'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-VENV_PATH = os.path.dirname(BASE_DIR)
-STATIC_ROOT = os.path.join(VENV_PATH, 'static_root')
+#VENV_PATH = os.path.dirname(BASE_DIR)
+#STATIC_ROOT = os.path.join(VENV_PATH, 'static_root')
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 
 django_heroku.settings(locals())
